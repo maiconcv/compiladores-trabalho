adf319ba91a5727a4fee2fc6c644176fdaf457dd
maiconcv <maicon_c_v@hotmail.com>
2019-08-24 22:53:52 -0300

    main prints line number

--- etapa1/main.c
+++ etapa1/main.c
@@ -1,40 +1,45 @@
 int main(){
 	int tok;
 	hashInit();
+	int lineno;
+
 	while(running){
 		tok = yylex();
+		lineno = getLineNumber();
 		if(!running)
 			break;
 		switch(tok){
-			case KW_BYTE: fprintf(stderr, "Keyword 'byte'.\n"); break;
-			case KW_INT: fprintf(stderr, "Keyword 'int'.\n"); break;
-			case KW_LONG: fprintf(stderr, "Keyword 'long'.\n"); break;
-			case KW_FLOAT: fprintf(stderr, "Keyword 'float'.\n"); break;
-			case KW_BOOL: fprintf(stderr, "Keyword 'bool'.\n"); break;
-			case KW_IF: fprintf(stderr, "Keyword 'if'.\n"); break;
-			case KW_THEN: fprintf(stderr, "Keyword 'then'.\n"); break;
-			case KW_ELSE: fprintf(stderr, "Keyword 'else'.\n"); break;
-			case KW_WHILE: fprintf(stderr, "Keyword 'while'.\n"); break;
-			case KW_FOR: fprintf(stderr, "Keyword 'for'.\n"); break;
-			case KW_READ: fprintf(stderr, "Keyword 'read'.\n"); break;
-			case KW_PRINT: fprintf(stderr, "Keyword 'print'.\n"); break;
-			case KW_RETURN: fprintf(stderr, "Keyword 'return'.\n"); break;
-			case KW_BREAK: fprintf(stderr, "Keyword 'break'.\n"); break;
-			case OPERATOR_LE: fprintf(stderr, "Operator '<='.\n"); break;
-			case OPERATOR_GE: fprintf(stderr, "Operator '>='.\n"); break;
-			case OPERATOR_EQ: fprintf(stderr, "Operator '=='.\n"); break;
-			case OPERATOR_DIF: fprintf(stderr, "Operator '!='.\n"); break;
-			case TK_IDENTIFIER: fprintf(stderr, "Identifier.\n"); break;
-			case LIT_INTEGER: fprintf(stderr, "Literal Integer.\n"); break;
-			case LIT_FLOAT: fprintf(stderr, "Literal Float.\n"); break;
-			case LIT_TRUE: fprintf(stderr, "Literal True.\n"); break;
-			case LIT_FALSE: fprintf(stderr, "Literal False.\n"); break;
-			case LIT_CHAR: fprintf(stderr, "Literal Char.\n"); break;
-			case LIT_STRING: fprintf(stderr, "Literal String.\n"); break;
-			case TOKEN_ERROR: fprintf(stderr, "Token error.\n"); break;
-			default: fprintf(stderr, "Token ascii %c\n", tok); break;
+			case KW_BYTE: fprintf(stderr, "Line %d: Keyword 'byte'.\n", lineno); break;
+			case KW_INT: fprintf(stderr, "Line %d: Keyword 'int'.\n", lineno); break;
+			case KW_LONG: fprintf(stderr, "Line %d: Keyword 'long'.\n", lineno); break;
+			case KW_FLOAT: fprintf(stderr, "Line %d: Keyword 'float'.\n", lineno); break;
+			case KW_BOOL: fprintf(stderr, "Line %d: Keyword 'bool'.\n", lineno); break;
+			case KW_IF: fprintf(stderr, "Line %d: Keyword 'if'.\n", lineno); break;
+			case KW_THEN: fprintf(stderr, "Line %d: Keyword 'then'.\n", lineno); break;
+			case KW_ELSE: fprintf(stderr, "Line %d: Keyword 'else'.\n", lineno); break;
+			case KW_WHILE: fprintf(stderr, "Line %d: Keyword 'while'.\n", lineno); break;
+			case KW_FOR: fprintf(stderr, "Line %d: Keyword 'for'.\n", lineno); break;
+			case KW_READ: fprintf(stderr, "Line %d: Keyword 'read'.\n", lineno); break;
+			case KW_PRINT: fprintf(stderr, "Line %d: Keyword 'print'.\n", lineno); break;
+			case KW_RETURN: fprintf(stderr, "Line %d: Keyword 'return'.\n", lineno); break;
+			case KW_BREAK: fprintf(stderr, "Line %d: Keyword 'break'.\n", lineno); break;
+			case OPERATOR_LE: fprintf(stderr, "Line %d: Operator '<='.\n", lineno); break;
+			case OPERATOR_GE: fprintf(stderr, "Line %d: Operator '>='.\n", lineno); break;
+			case OPERATOR_EQ: fprintf(stderr, "Line %d: Operator '=='.\n", lineno); break;
+			case OPERATOR_DIF: fprintf(stderr, "Line %d: Operator '!='.\n", lineno); break;
+			case TK_IDENTIFIER: fprintf(stderr, "Line %d: Identifier.\n", lineno); break;
+			case LIT_INTEGER: fprintf(stderr, "Line %d: Literal Integer.\n", lineno); break;
+			case LIT_FLOAT: fprintf(stderr, "Line %d: Literal Float.\n", lineno); break;
+			case LIT_TRUE: fprintf(stderr, "Line %d: Literal True.\n", lineno); break;
+			case LIT_FALSE: fprintf(stderr, "Line %d: Literal False.\n", lineno); break;
+			case LIT_CHAR: fprintf(stderr, "Line %d: Literal Char.\n", lineno); break;
+			case LIT_STRING: fprintf(stderr, "Line %d: Literal String.\n", lineno); break;
+			case TOKEN_ERROR: fprintf(stderr, "Line %d: Token error.\n", lineno); break;
+			default: fprintf(stderr, "Line %d: Token ascii '%c'.\n", lineno, tok); break;
 		}	
 	}
+
+	printf("\nTotal lines: %d\n\n", lineno);
 	hashPrint();
 }
 
