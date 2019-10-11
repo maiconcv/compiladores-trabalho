5e74bb2b045d0a88e01dfd471b06c4cbeb554368
maiconcv <maicon_c_v@hotmail.com>
2019-10-11 16:44:05 -0300

    hash does not insert if value already inserted

--- etapa4/hash.c
+++ etapa4/hash.c
@@ -17,14 +17,20 @@ int hashAddress(char* text){
 }
 
 HASH_NODE* hashFind(char* text){
-	return NULL;
+	int address = hashAddress(text);
+	for(HASH_NODE* node = Table[address]; node; node = node->next)
+		if(!strcmp(text, node->text))
+			return node;
+	return 0;
 }
 
-HASH_NODE* hashInsert(char* text){
+HASH_NODE* hashInsert(char* text, int type){
 	HASH_NODE* newnode;
+	if((newnode = hashFind(text)) != 0)
+		return newnode;
 	int address = hashAddress(text);
 	newnode = (HASH_NODE*)calloc(1, sizeof(HASH_NODE));
-	newnode->type = 1;
+	newnode->type = type;
 	newnode->text = (char*)calloc(strlen(text) + 1, sizeof(char));
 	strcpy(newnode->text, text);
 	newnode->next = Table[address];
--- etapa4/hash.h
+++ etapa4/hash.h
@@ -7,6 +7,14 @@
 
 #define HASH_SIZE 997
 
+#define SYMBOL_IDENTIFIER 1
+#define SYMBOL_LITINT 2
+#define SYMBOL_LITREAL 3
+#define SYMBOL_LITTRUE 4
+#define SYMBOL_LITFALSE 5
+#define SYMBOL_LITCHAR 6
+#define SYMBOL_LITSTRING 7
+
 typedef struct hash_node {
 	int type;
 	char* text;
@@ -16,7 +24,7 @@ typedef struct hash_node {
 void hashInit(void);
 int hashAddress(char* text);
 HASH_NODE* hashFind(char* text);
-HASH_NODE* hashInsert(char* text);
+HASH_NODE* hashInsert(char* text, int type);
 void hashPrint(void);
 
 #endif // COMP_HASH_H
